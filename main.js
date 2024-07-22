6d06e2671a03ecd6612c5e6470d0de06989cfbdc
Dean Hope <56652777+deanhopes@users.noreply.github.com>
2024-07-22 17:43:23 +0100

    added html

--- main.js
+++ main.js
@@ -1,24 +1,15 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import "./style.css";
+import { gsap } from "gsap";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
+document.querySelector("#app").innerHTML = /*html*/`
+  <div class="container">
+    <nav>
+      <a href="#" id="logo">Beanos</a>
+      <a href="#">Subscribe</a>
+    </nav>
+    <footer>
+      <a href="#">Unlock Source Code with Pro</a>
+      <a href="#">Link in description</a>
+    </footer>
   </div>
-`
-
-setupCounter(document.querySelector('#counter'))
+`;
